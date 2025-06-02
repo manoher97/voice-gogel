868e068827542b02ab098ff9aabb7895bf43acc2
Kumar <149046952+manoher97@users.noreply.github.com>
2025-06-02 14:36:29 +0530

    This is Booking file update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Voice from './Home';
+import React, { useState } from 'react';
+import BookingSystem from './components/Registration-form';
+import AdminPanel from './components/Addmin-panel';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [bookings, setBookings] = useState([]);
+  const [view, setView] = useState('user');
+
+  function handleNewBooking(booking) {
+    setBookings(prev => [...prev, booking]);
+  }
+
   return (
-    <div className="App"> 
-      <Voice />
+    <div className='App-r'>
+      <div style={{ marginBottom: '1rem' }}>
+        <button  onClick={() => setView('user')} className='btn'>User View</button>
+        <button  onClick={() => setView('admin')} className='btn'>Admin Panel</button>
+      </div>
+
+      {view === 'user' ? (
+       <BookingSystem onSubmitBooking={handleNewBooking} />
+      ) : (
+        <AdminPanel bookings={bookings} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
+
